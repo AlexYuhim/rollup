f446a554ff7b2ea70b2407e3b8553015795ccd64
AlexYuhim <88594227+AlexYuhim@users.noreply.github.com>
2021-11-08 23:32:39 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "../index.css"
+
+console.log('hello word!!!')
+
+const jsImgHtml = document.createElement('img')
+jsImgHtml.classList = 'js-imeg'
+jsImgHtml.src = '../assets/JSImg.jpg'
+
+document.body.append(jsImgHtml)
